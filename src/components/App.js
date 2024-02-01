0fb9be819dd9f02a1d8f7ea15652577071b81246
Joshua Sol <joshuasol4950@gmail.com>
2024-02-01 11:41:30 +0800

    progress

--- src/components/App.js
+++ src/components/App.js
@@ -6,62 +6,67 @@ import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Questions from "./Questions";
 import NextButton from "./NextButton";
+import Progress from "./Progress";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    case "start":
+      return {
+        ...state,
+        status: "active",
+      };
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+
+    case "nextQuestion":
+      return {
+        ...state,
+        index: state.index + 1,
+        answer: null,
+      };
+    default:
+      throw new Error("Action unknown");
+  }
+}
 
 export default function App() {
-  const initialState = {
-    questions: [],
-    status: "loading",
-    index: 0,
-    answer: null,
-    points: 0,
-  };
-
-  function reducer(state, action) {
-    switch (action.type) {
-      case "dataReceived":
-        return {
-          ...state,
-          questions: action.payload,
-          status: "ready",
-        };
-      case "dataFailed":
-        return {
-          ...state,
-          status: "error",
-        };
-      case "start":
-        return {
-          ...state,
-          status: "active",
-        };
-      case "newAnswer":
-        const question = state.questions.at(state.index);
-        return {
-          ...state,
-          answer: action.payload,
-          points:
-            action.payload === question.correctOption
-              ? state.points + question.points
-              : state.points,
-        };
-
-      case "nextQuestion":
-        return {
-          ...state,
-          index: state.index + 1,
-          answer: null,
-        };
-      default:
-        throw new Error("Action unknown");
-    }
-  }
-
-  const [{ questions, status, index, answer }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
     reducer,
     initialState
   );
 
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
@@ -80,6 +85,13 @@ export default function App() {
         )}
         {status === "active" && (
           <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
             <Questions
               question={questions[index]}
               dispatch={dispatch}
